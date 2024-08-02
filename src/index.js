31f39a282f4c4aa013cc14e792eae0f191841ab1
Bruno <brunorsousa1998@gmail.com>
2024-08-01 21:10:56 -0300

    Modifica configuração da aplicação

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('express')();
 
-app.get('/', (_req, res) => res.json({ message: 'Hi' }));
+app.get('/', (_req, res) => res.json({ message: 'Hello again' }));
 
 app.listen(3000, () => console.log('App is running at http://127.0.0.1:3000'));
